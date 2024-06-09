1e3349676f1e5c9a7ab02a872ea0598eb133bc54
Dariya Kashtan <kashtandariya@gmail.com>
2024-06-09 14:15:20 +0200

    main/classe prodotto
    impostazione iniziale classe prodotto con attributi privati, costruttore, incapsulamento

---
+++ src/org/shop/Prodotto.java
@@ -0,0 +1,58 @@
+package org.shop;
+
+public class Prodotto {
+	    private int codice;
+	    private String nome;
+	    private String marca;
+	    private double prezzo;
+	    private double iva;
+
+	    public Prodotto(int codice, String nome, String marca, double prezzo, double iva) {
+	        this.codice = codice;
+	        this.nome = nome;
+	        this.marca = marca;
+	        this.prezzo = prezzo;
+	        this.iva = iva;
+	    }
+
+	    //codice solo lettura
+	    public int getCodice() {
+	        return codice;
+	    }
+
+	    // Getter e setter per nome
+	    public String getNome() {
+	        return nome;
+	    }
+
+	    public void setNome(String nome) {
+	        this.nome = nome;
+	    }
+
+	    // Getter e setter per marca
+	    public String getMarca() {
+	        return marca;
+	    }
+
+	    public void setMarca(String marca) {
+	        this.marca = marca;
+	    }
+
+	    // Getter e setter per prezzo
+	    public double getPrezzo() {
+	        return prezzo;
+	    }
+
+	    public void setPrezzo(double prezzo) {
+	        this.prezzo = prezzo;
+	    }
+
+	    // Getter e setter per iva
+	    public double getIva() {
+	        return iva;
+	    }
+
+	    public void setIva(double iva) {
+	        this.iva = iva;
+	    }
+}
